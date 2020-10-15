49d48994a86c532a3a7461f19647969e432ea10a
Nina Le <le.ngan@husky.neu.edu>
2020-10-15 08:35:39 -0700

    interval list intersection

--- src/Intervals/986_Interval_List_Intersections.py
+++ src/Intervals/986_Interval_List_Intersections.py
@@ -1,1 +1,20 @@
-def
+def intervalIntersection(A, B):
+    res = []
+    i = 0
+    j = 0
+
+    while i < len(A) and j < len(B):
+        lo = max(A[i][0], B[j][0])
+        hi = min(A[i][1], B[j][1])
+
+        if lo <= hi:
+            res.append([lo,hi])
+        
+        if A[i][1] < B[j][1]:
+            i += 1
+        else:
+            j += 1
+    return res
+
+# Complexity: O(n+m) 
+# Space: O(m+n) to store
