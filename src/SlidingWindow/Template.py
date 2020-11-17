2b515d6b260ec556813eddccef337ce64b9eeb6b
Mike Tran <ngocmike238@gmail.com>
2020-11-17 02:19:07 -0500

    Add template

---
+++ src/SlidingWindow/Template.py
@@ -0,0 +1,15 @@
+def Template():
+    #Two pointers i,j (i <= j)
+    #A HashMap/Set/Counter 
+    i = 0
+    for j in range (0, len(array)) :
+        #Update counter/map/set
+        while ( [i,j] invalid/valid ) :
+            #//Do something to make it valid/invalid
+            #Update counter/map/set
+            #Window size = j - i +1 
+            i += 1
+        }
+        #Update final result 
+    }
+
